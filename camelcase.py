6d7981a33ff22301e6cf208125059d6689695d94
Vitalii Krashchenko <vkrashchenko@vkrashchenko-ws-krakow.hw.griddynamics.net>
2017-12-19 15:37:37 +0100

    added solution for camelcase

---
+++ camelcase.py
@@ -0,0 +1,15 @@
+#!/bin/python
+## Problem: https://www.hackerrank.com/challenges/camelcase/problem
+
+import sys
+
+customText = "FistThingsTheSea"
+
+s = customText
+wordCounter = 0
+if len(s) > 0:
+    wordCounter = 1
+for i in xrange(1, len(s)):
+    if ord(s[i]) >= ord('A') and ord(s[i]) <= ord('Z'):
+        wordCounter += 1
+print wordCounter
